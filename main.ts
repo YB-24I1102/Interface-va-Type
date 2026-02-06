443f8807b7981d81fc45b73dbe805dbd6baa822f
saferboy <muxtordev@gmail.com>
2026-02-06 16:25:29 +0500

    kim ochirsa eshak

---
+++ main.ts
@@ -0,0 +1,55 @@
+import { log } from "console";
+
+let names: string = "John Doe";
+let age: number = 30;
+let price: number = 19.99;
+
+let isActive: boolean;
+
+// Array
+
+let numbers: number[] = [1, 2, 3, 4, 5, 6];
+let fruits: string[] = ["Apple", "Banana", "Cherry"];
+let bulen: Array<boolean> = [true, false, true];
+
+let mixed: [number, string, boolean] = [1, "Hello", true];
+let none: any = false;
+
+function greet(user: string): string {
+  return user;
+}
+
+// interface User {
+//   id?: number;
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// }
+
+// const user: User = {
+//     // id: 12,
+//     name: "John Doe",
+//     email: "john.doe@example.com",
+//     isActive: true
+// }
+
+interface BaseUser {
+  id: number;
+  name: string;
+}
+type Student = string | number | boolean;
+
+interface Admin extends BaseUser {
+  role: string;
+  email: string;
+  student?: Student;
+}
+
+const user: Admin = {
+  id: 1,
+  name: "John Doe",
+  role: "Admin",
+  email: "john.doe@example.com",
+};
+
+console.log(user);
--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+import http from "http";
+
+const user = {
+    id: 1,
+    name: "Ali",
+    age: 25
+}
+
+"/user"
+"/users"
+
+
+const server = http.createServer((request, response) => {
+  if (request.url === "/hello") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: "Salom! Bu backend javobi." }));
+    return;
+    }
+    
+    if (request.url === "/salom") {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify({ message: "Xayr! Bu backend javobi." }));
+      return;
+    }
+
+  response.writeHead(404, { "Content-Type": "text/plain" });
+  response.end("Sahifa topilmadi");
+});
+
+server.listen(3000, () => {
+  console.log("Server 3000-portda ishga tushdi");
+});
